3061e9f0ac4aee4081b70c94196bdc6f43f03b00
dylanmejia <dylanmejia004@gmail.com>
2024-02-01 12:49:00 -0500

    Initial commit

--- src/main/java/com/mycompany/csc229_assignment01/Course.java
+++ src/main/java/com/mycompany/csc229_assignment01/Course.java
@@ -1,31 +1,3 @@
-/*
-* ToDo 01:
- Complete the followings:
-
-The Course class should have:
-
-    - ID 
-
-    - Name
-
-    - Code
-
- - You need to have setters and getters for all data members
- - You must have a default constructor and an overloaded one that accepts 3 parameters to update the member variables.
-
- - Using a driver class (with main), instantiate the Course class and call its methods to change the id, name, and code.
-
- - Test and run your code.
-
- - Add comments to explain your program.
-
- - Push the code to GitHub.
-
- - Submit the Github link here (make sure it is a public repo).
-
-
- */
-
 package com.mycompany.csc229_assignment01;
 
 /**
@@ -33,5 +5,16 @@ package com.mycompany.csc229_assignment01;
  * @author MoaathAlrajab
  */
 public class Course {
+    private String name;
+    private int ID;
+    private
 
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
 }
+
+
--- src/main/java/com/mycompany/csc229_assignment01/NewClass.java
+++ src/main/java/com/mycompany/csc229_assignment01/NewClass.java
@@ -1,4 +1,3 @@
-
 package com.mycompany.csc229_assignment01;
 
 /**
@@ -8,7 +7,7 @@ package com.mycompany.csc229_assignment01;
 public class NewClass {
     
      public static void main(String[] args) {
-        System.out.println("Hello World!");
+         System.out.println("Hello World!");
     }
     
 }
